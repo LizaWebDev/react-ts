48d1af05a0b0c8dd141424bbb8f580edf6433720
LizaWebDev <e.kuntsevich03@gmail.com>
2023-01-24 23:30:42 +0300

    components (part 2)

--- src/App.tsx
+++ src/App.tsx
@@ -5,44 +5,70 @@ import './App.css';
 function App() {
     // полезное что-то
     // обязана вернуть JSX
+    console.log("App rendering")
     return (
         <div className="App">
-            This is App component
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
     );
+}
+
+function AppTitle() {
+    console.log("AppTitle rendering")
+    return (
+        <>This is App component</>
+    )
 }
 
 function Rating() {
+    console.log("Rating rendering")
     return (
-        <div>
+        <>
             <Star/>
             <Star/>
             <Star/>
             <Star/>
             <Star/>
-        </div>
+        </>
 
     );
 }
 
-function Star () {
+function Star() {
+    console.log("Star rendering")
     return (
         <div>star</div>
     );
 }
 
 function Accordion() {
+    console.log("Accordion rendering")
     return (
         <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
+    );
+}
+
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
+    return (
+        <h3>Menu</h3>
+    );
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+    return (
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
     );
 }
 
